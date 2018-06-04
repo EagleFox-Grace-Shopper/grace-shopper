ac5cd9638c1d9ecf4c11f2affce4710f79392e28
chansiky <chansiky@gmail.com>
2018-06-04 17:25:33 -0500

    User model defined

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -26,6 +26,13 @@ const User = db.define('user', {
   },
   googleId: {
     type: Sequelize.STRING
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  addresss: {
+    type: Sequelize.String,
   }
 })
 

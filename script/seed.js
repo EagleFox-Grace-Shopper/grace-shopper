e51f19f2f3a9a71dc054810698736cc3219626a7
J-Hust <33457091+J-Hust@users.noreply.github.com>
2018-06-05 16:19:30 -0500

    complete seedfile

--- script/seed.js
+++ script/seed.js
@@ -1,26 +1,13 @@
 'use strict'
 
 const db = require('../server/db')
-const {User, Product, Category} = require('../server/db/models')
+const {User, Product, Category, productCategory} = require('../server/db/models')
 
-
-/**
- * Welcome to the seed file! This seed file uses a newer language feature called...
- *
- *                  -=-= ASYNC...AWAIT -=-=
- *
- * Async-await is a joy to use! Read more about it in the MDN docs:
- *
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
- *
- * Now that you've got the main idea, check it out in practice below!
- */
 
 async function seed () {
   await db.sync({force: true})
   console.log('db synced!')
-  // Whoa! Because we `await` the promise that db.sync returns, the next line will not be
-  // executed until that promise resolves!
+
   const users = await Promise.all([
     User.create({name: 'Chan', email: 'chan@email.com', password: '123', address: '123 Main Street, Chicago, IL 94597'}),
     User.create({name: 'Andrew Trahan', email: 'andrew@email.com', password: '12345', address: '123 Wicker Street, Chicago, IL 94597'}),
@@ -28,16 +15,89 @@ async function seed () {
     User.create({name: 'Justin H', email: 'justin@email.com', password: 'puppies123', address: '3006 W Fullerton Ave, Chicago, IL 60647'}),
   ])
 
-  const products = await Promise.all([
-    Product.create({title: '',
-      description: '',
-      price: '',
-      quantity: '',
-      imageUrl: ''})
+  const [wacky, food, clothing, kitchen] = await Promise.all([
+    Category.create({name: 'wacky'}),
+    Category.create({name: 'food'}),
+    Category.create({name: 'clothing'}),
+    Category.create({name: 'kitchen'}),
   ])
-  // Wowzers! We can even `await` on the right-hand side of the assignment operator
-  // and store the result that the promise resolves to in a variable! This is nice!
+
+  const [crocksocks, grillba, licki, sauna, dunker, mug, nightstand, tube, lettuce, handitaur] = await Promise.all([
+    Product.create({title: 'Crocs with socks',
+      description: 'These crocs have built in socks!',
+      price: 19.99,
+      quantity: 43,
+      imageUrl: 'https://i.redd.it/nyjqg5zjas111.png'}),
+    Product.create({title: 'Grillba',
+      description: 'Roomba for your grill',
+      price: 49.99,
+      quantity: 87,
+      imageUrl: 'https://i.redd.it/287mfbuf7x111.gif'}),
+    Product.create({title: 'Licki Brush',
+      description: 'Lick your cat!',
+      price: 15.99,
+      quantity: 62,
+      imageUrl: 'https://i.redd.it/orqpr3q0vl111.jpg'}),
+    Product.create({title: 'Rejuvenator Portable Sauna',
+      description: 'The finest airline experience possible',
+      price: 149.99,
+      quantity: 22,
+      imageUrl: 'https://imgur.com/8gTABmt'}),
+    Product.create({title: 'Magnetic Cookie Dunker',
+      description: 'One step closer to a world without war',
+      price: 9.99,
+      quantity: 103,
+      imageUrl: 'http://i.imgur.com/FQR4nrT.jpg?1'}),
+    Product.create({title: 'The Floating Mug',
+      description: 'No more coasters!',
+      price: 9.99,
+      quantity: 149,
+      imageUrl: 'https://tinyurl.com/ybs3ydxr'}),
+    Product.create({title: 'Bat and Shield Night Stand',
+      description: 'Night stand that turns into a bat and shield',
+      price: 59.99,
+      quantity: 52,
+      imageUrl: 'https://i.redd.it/12sxmkwqovr01.jpg'}),
+    Product.create({title: 'Experience Tube',
+      description: 'Never be distracted by your phone again',
+      price: 29.99,
+      quantity: 89,
+      imageUrl: 'https://i.redd.it/0u0xt3ymumzz.jpg'}),
+    Product.create({title: 'Lettuce Umbrella',
+      description: 'An umbrella that looks like lettuce.  (Not edible)',
+      price: 24.99,
+      quantity: 12,
+      imageUrl: 'https://i.redd.it/ck0u2asgcvw01.jpg'}),
+    Product.create({title: 'Handitaur',
+      description: 'A centaur for your hand',
+      price: 12.99,
+      quantity: 40,
+      imageUrl: 'https://i.redd.it/ja8qovp7qukz.jpg'}),
+  ])
+
+  const productCategoryData = [
+    {productId: crocksocks.id, categoryId: clothing.id},
+    {productId: crocksocks.id, categoryId: wacky.id},
+    {productId: grillba.id, categoryId: kitchen.id},
+    {productId: licki.id, categoryId: clothing.id},
+    {productId: licki.id, categoryId: wacky.id},
+    {productId: sauna.id, categoryId: wacky.id},
+    {productId: dunker.id, categoryId: kitchen.id},
+    {productId: dunker.id, categoryId: food.id},
+    {productId: mug.id, categoryId: kitchen.id},
+    {productId: mug.id, categoryId: food.id},
+    {productId: nightstand.id, categoryId: wacky.id},
+    {productId: tube.id, categoryId: wacky.id},
+    {productId: lettuce.id, categoryId: wacky.id},
+    {productId: lettuce.id, categoryId: food.id},
+    {productId: handitaur.id, categoryId: wacky.id},
+    {productId: handitaur.id, categoryId: clothing.id},
+  ]
+  await db.models.productCategory.bulkCreate(productCategoryData)
+
+
   console.log(`seeded ${users.length} users`)
+  // console.log(`seeded ${products.length} products and ${categories.length} categories`)
   console.log('seeded successfully')
 }
 
@@ -50,16 +110,12 @@ if (module === require.main) {
       console.error(err)
       process.exitCode = 1
     })
-    .then(() => { // `finally` is like then + catch. It runs no matter what.
+    .then(() => {
       console.log('closing db connection')
       db.close()
       console.log('db connection closed')
     })
-  /*
-   * note: everything outside of the async function is totally synchronous
-   * The console.log below will occur before any of the logs that occur inside
-   * of the async function
-   */
+
   console.log('seeding...')
 }
 

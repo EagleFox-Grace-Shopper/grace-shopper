828507a67f68f23b1ca9924cc70d2eb6e80cda2f
AndrewTrahan <globalART19@gmail.com>
2018-06-05 14:57:32 -0500

    Redux product pieces are initialized

--- client/store/index.js
+++ client/store/index.js
@@ -1,15 +1,17 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import createLogger from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import user from './user'
+import product from './product'
 
-const reducer = combineReducers({user})
+const reducer = combineReducers({ user })
 const middleware = composeWithDevTools(applyMiddleware(
   thunkMiddleware,
-  createLogger({collapsed: true})
+  createLogger({ collapsed: true })
 ))
 const store = createStore(reducer, middleware)
 
 export default store
 export * from './user'
+export * from './product'

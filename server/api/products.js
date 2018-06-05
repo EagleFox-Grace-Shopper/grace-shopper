4aa9cb1fc1b823ebff2224636f1772f85b3e4df6
chansiky <chansiky@gmail.com>
2018-06-05 15:35:14 -0500

    Products api routes

--- server/api/products.js
+++ server/api/products.js
@@ -1,27 +1,28 @@
 //api get for product info
-const router = require('express').Router();
-const db = require('../db')
-const Product = require('../db/models/product')
+const router = require('express').Router()
+const {Product} = require('../db/models')
 
 
-router.get('/', (req,res, next) => { 
-  try{
-    const productsAll = Product.findAll();
-    res.json(productsAll);
-  }catch(error){
+router.get('/', async (req, res, next) => {
+  try {
+    const productsAll = await Product.findAll({})
+    res.json(productsAll)
+  } catch (error){
     next(error)
   }
-}
+})
 
-router.get('/:id', (req,res,next) => {
-  try{
-    const productById = Product.findById(req.params.id);
-    res.json(productById);
-  }catch(error){
+router.get('/:id', async (req, res, next) => {
+  try {
+    const productById = await Product.findById(req.params.id)
+    res.json(productById)
+  } catch (error){
     next(error)
   }
-}
+})
 
-router.post('/:id', (req,res,next) =>{
+router.post('/:id', async (req, res, next) =>{
   //
-}
+})
+
+module.exports = router

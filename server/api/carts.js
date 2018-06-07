5c205ec4b8d9cd33e1480c7269ad19a8cae775d7
chansiky <chansiky@gmail.com>
2018-06-07 14:09:10 -0500

    carts routes

--- server/api/carts.js
+++ server/api/carts.js
@@ -2,41 +2,50 @@ const express = require('express')
 const router = express.Router()
 const {CartItem, Product} = require('../db/models')
 
-//route gets all the items in a user's cart
-router.get('/:userId', async (req,res,next) => {
+//route to get all the items in the cart
+router.get('/:userId', async (req, res, next) => {
   const cartItems = await CartItem.findAll({
     where: {
       userId: req.params.userId,
     }
   })
+  res.json(cartItems)
 })
 
-//route posts an item to a user's cart
-router.post('./userId' async (req,res,next) => {
+//route posts and item to a user's cart
+router.post('/:userId', async (req, res, next) => {
   //route should post an item into the database
   //what is the best way to pass in the item number and quantity?
   ////in this case we pass it in the req.body
 
   //make sure productId refers to an existing product
-  try{
+  try {
     const product = Product.findById(req.body.productId)
-    const quantity = req.body.quantity ?
+    const productQuantity = req.body.quantity ?
       req.body.quantity :
       1
-    //create the cart item with association, consider using build:
-    ////does it need to be saved into the database everytime we query
-    ////this post request, or can we trigger a save to database request
-    ////somewhere else?
+    console.log(productQuantity)
     const cartItem = await CartItem.create(
       {
-         
+        where: {
+          userId: req.params.userId,
+          productId: product.dataValue.id,
+        },
+        defaults: {
+          quantity: productQuantity
+        }
       }
     )
-    return cartItem
-  }catch(e){
-    next(e);
+    res.json(cartItem[0])
+  } catch (error){
+    next(error)
   }
 
-}
+  //create the cart item with association, consider using build:
+  ////does it need to be saved into the database everytime we query
+  ////this post request, or can we trigger a save to database request
+  ////somewhere else?
+})
+
 
 module.exports = router

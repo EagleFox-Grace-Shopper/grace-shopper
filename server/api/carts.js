b887e36ef9dca5bf17778aef8e99da4ff4044303
chansiky <chansiky@gmail.com>
2018-06-07 16:04:48 -0500

    routes for cart: get cart, add to cart, update cart

--- server/api/carts.js
+++ server/api/carts.js
@@ -7,45 +7,121 @@ router.get('/:userId', async (req, res, next) => {
   const cartItems = await CartItem.findAll({
     where: {
       userId: req.params.userId,
-    }
+    },
+    include: [{
+      model: Product,
+    }]
   })
   res.json(cartItems)
 })
 
-//route posts and item to a user's cart
-router.post('/:userId', async (req, res, next) => {
-  //route should post an item into the database
-  //what is the best way to pass in the item number and quantity?
-  ////in this case we pass it in the req.body
+/*
+/api/carts/:userId/add
+adds a product of a specified quantity to the cart.  If the product
+already exists in the cart for the user, this adds the specified quantity
 
-  //make sure productId refers to an existing product
+route posts item to a user's cart
+  (using userId as cart identification in req.params)
+example req.body is:
+  {
+    productId : 3,
+    quantity : 1,
+  }
+
+  or
+
+  {
+    productId : 3,
+  }
+*/
+
+router.post('/:userId/add', async (req, res, next) => {
   try {
-    const product = Product.findById(req.body.productId)
+    const product = await Product.findById(req.body.productId)
+    console.log('product', product)
+    if (!product){
+      throw new Error('product of specified id does not exist')
+    }
     const productQuantity = req.body.quantity ?
-      req.body.quantity :
+      Number(req.body.quantity) :
       1
-    console.log(productQuantity)
-    const cartItem = await CartItem.create(
+    const cartItem = await CartItem.findOrCreate(
       {
         where: {
           userId: req.params.userId,
-          productId: product.dataValue.id,
+          productId: product.dataValues.id,
         },
         defaults: {
           quantity: productQuantity
         }
       }
     )
+
+    if (cartItem[1] === false){
+      CartItem.update(
+        {
+          quantity: productQuantity + cartItem[0].quantity,
+        },
+        {
+          where: {
+            id: cartItem[0].id,
+          }
+        },
+      )
+    }
+
     res.json(cartItem[0])
   } catch (error){
     next(error)
   }
-
-  //create the cart item with association, consider using build:
-  ////does it need to be saved into the database everytime we query
-  ////this post request, or can we trigger a save to database request
-  ////somewhere else?
 })
 
+
+/*
+/api/carts/:userId/update
+updates a product to a specified quantity
+
+example req.body is:
+  {
+    productId : 3,
+    quantity : 1,
+  }
+*/
+
+router.put('/:userId/update', async (req, res, next) => {
+  try {
+    /*
+    const product = await Product.findById(req.body.productId)
+    console.log('product', product)
+    if (!product){
+      throw new Error('product of specified id does not exist')
+    }
+    */
+    const productQuantity = Number(req.body.quantity)
+    if (!productQuantity){
+      throw new Error('"quantity" not specified in req.body')
+    }
+
+    const cartItem = await CartItem.findOne({
+      where: {
+        userId: req.params.userId,
+        productId: req.body.productId,
+      }
+    })
+
+    if (cartItem){
+      const cartItemUpdated = CartItem.update(
+        {quantity: productQuantity},
+        {where: {id: cartItem.id}},
+      )
+      res.json(cartItemUpdated)
+    }
+    else {
+      throw new Error('cartItem does not exist in database')
+    }
+  } catch (error){
+    next(error)
+  }
+})
 
 module.exports = router

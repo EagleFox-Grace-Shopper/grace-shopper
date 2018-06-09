ac0048f2419f6381208427ff82392eae8d32454a
J-Hust <33457091+J-Hust@users.noreply.github.com>
2018-06-09 14:02:30 -0500

    update categoryList

--- client/components/category-list.js
+++ client/components/category-list.js
@@ -5,25 +5,28 @@ import {Link} from 'react-router-dom'
 
 
 const categoryList = (props) => {
+  console.log('categ', props.categories)
   const cats = props.categories
   return (
     <div>
       <table>
-        <tr>
-          <th />
-          <th>Name</th>
-          <th />
-          <th />
-        </tr>
-        {cats.map(cat => {
+        <tbody>
           <tr>
-            <td>
-              <input disabled type="text" value={cat.name} />
-            </td>
-            <td>Edit</td>
-            <td>Remove</td>
+            <th />
+            <th>Name</th>
+            <th />
+            <th />
           </tr>
-        })}
+          {cats.map(cat => {
+            <tr>
+              <td>
+                <input disabled type="text" value={cat.name} />
+              </td>
+              <td>Edit</td>
+              <td>Remove</td>
+            </tr>
+          })}
+        </tbody>
       </table>
     </div>
   )
@@ -31,7 +34,10 @@ const categoryList = (props) => {
 
 const mapStateToProps = props => {
   return {
-    categories: state.category.categories
+    // categories: state.category.categories
+    categories: [{name: 'hello'}, {name: 'pizza'}, {name: 'beer'}, {name: 'xmas'}]
   }
 }
+
+export default connect(mapStateToProps)(categoryList)
 
--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { withRouter, Route, Switch } from 'react-router-dom'
 import PropTypes from 'prop-types'
-import { Login, Signup, UserHome, AddProduct, EditProduct, SingleProduct, ProductList } from './components'
+import { Login, Signup, UserHome, AddProduct, EditProduct, SingleProduct, ProductList, categoryList } from './components'
 import { me } from './store'
 
 /**
@@ -22,7 +22,7 @@ class Routes extends Component {
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         <Route exact path="/" component={ProductList} />
-
+        <Route exact path="/catlist" component={categoryList} />
         {
           isAdmin &&
           <Route exact path="/products/add" component={AddProduct} />

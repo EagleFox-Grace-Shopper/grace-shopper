2c21ee4f4d7f7e4c16aee027cf2bb82dae17c1bf
chansiky <chansiky@gmail.com>
2018-06-10 19:21:38 -0500

    small change

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { logout } from '../store'
-import { getProductsByCatThunk, getProductsBySearchThunk} from '../store/product'
+import { getProductsByCatThunk, getProductsBySearchThunk } from '../store/product'
 import { CartIcon } from './index'
 import styled from 'styled-components'
 

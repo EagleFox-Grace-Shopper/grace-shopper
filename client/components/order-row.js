617806bfb13a01d3cfcc441d658f6b0e667c7e53
Homum Ahsan <hahsan95@gmail.com>
2018-06-12 14:31:37 -0500

    order row css

--- client/components/order-row.js
+++ client/components/order-row.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const TableRow = styled.tr`
+  border: 1px solid rgba(255, 193, 128, 1);
+  width: 100%;
+  margin: .25em;
+`
+
 export default function OrderRow(props) {
   const order = props.order
   return (
-    <div>
-      <h3>{order.id}</h3>
-      <h3>Total Amount: {order.totalAmount}</h3>
-      <h3>Quantity</h3>
-    </div>
+    <TableRow>
+      <td>{order.id}</td>
+      <td>{order.createdAt}</td>
+      <td>{order.totalAmount}</td>
+      <td>Status</td>
+    </TableRow>
   )
 }
 

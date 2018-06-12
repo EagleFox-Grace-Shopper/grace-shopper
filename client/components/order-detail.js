cb2a3417fcad727953e9b550eab0c96f09ca6d5c
Homum Ahsan <hahsan95@gmail.com>
2018-06-12 15:31:05 -0500

    single order page

--- client/components/order-detail.js
+++ client/components/order-detail.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react'
+import { getOrderThunk } from '../store'
+import { connect } from 'react-redux'
 
 class OrderDetail extends Component {
+
+  async componentDidMount() {
+    if (this.props.selectedOrder.length === 0) {
+      await this.props.getOrderThunk()
+    }
+  }
+
   render() {
+
+    const selectedOrder = this.props.selectedOrder
     return (
       <div>
-        <h3>Title</h3>
+        <h3>{selectedOrder.title}</h3>
         <h3>Price</h3>
         <h3>Quantity</h3>
       </div>
     )
   }
 }
+
+const mapStateToProps = (store) => {
+  return {
+    selectedOrder: store.order.selectedOrder
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getOrderThunk: () => dispatch(getOrderThunk())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(OrderDetail)
